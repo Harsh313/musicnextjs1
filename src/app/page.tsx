3817b95776134763cbb54eb397c414e2bc84ddf2
Harsh313 <harsh313008@gmail.com>
2024-08-15 00:54:58 +0530

    project completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinar";
+import WhyChooseUS from "@/components/WhyChooseUS";
 
 
 export default function Home() {
@@ -8,6 +12,10 @@ export default function Home() {
       
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUS/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
     </main>
   );
 }
